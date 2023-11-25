c989b83bbab7388f9db654c4daee0ca35132e0ea
Ahmad Faiz Agustianto <107536877+Mifaki@users.noreply.github.com>
2023-11-26 01:59:40 +0700

    `feat`: Add Organizer Home Page

---
+++ client/src/pages/admin/Home.vue
@@ -0,0 +1,131 @@
+<template>
+  <q-layout view="hHh lpR fF2">
+    <q-page>
+      <img src="/images/home-banner.jpg" alt="Banner" class="home-banner">
+      <div class="main-container">
+        <div class="card-container">
+          <img src="/images/add-event.png" alt="Add Competition" class="add-btn q-mt-xl q-pt-xl">
+          <img src="/images/add-competition.png" alt="Add Event" class="add-btn q-mt-xl q-pt-xl">
+          <div v-if="(!event || compe) && competitionData.length > 0" v-for="compe in competitionData" :key="compe.idCompetition" class="card q-mt-xl">
+            <img src="/images/card-thumbnail.png" :alt="compe.competitionName + ' Thumbnail'">
+            <div class="text-gap">
+              <p class="jakarta-b text-lg">{{ compe.competitionName }}</p>
+              <p class="jakarta-r">Kategori: <span class="jakarta-b">{{ compe.competitionCategory }}</span></p>
+              <p class="jakarta-r">Jenjang: <span class="jakarta-b">{{ compe.jenjang }}</span></p>
+              <p class="jakarta-r">Tingkat: <span class="jakarta-b">{{ compe.tingkat }}</span></p>
+              <p class="jakarta-r">Kapasitas Tersisa: <span class="jakarta-b">{{ compe.capacityTersisa }}</span></p>
+            </div>
+            <q-btn color="primary" icon-right="chevron_right" label="Detail" />
+          </div>
+          <div v-if="(!compe || event) && academicData.length > 0" v-for="academic in academicData" :key="academic.idAcademicEvents" class="card q-mt-xl">
+            <img src="/images/card-thumbnail.png" :alt="academic.eventsName + ' Thumbnail'">
+            <div class="text-gap">
+              <p class="jakarta-b text-lg">{{ academic.eventsName }}</p>
+              <p class="jakarta-r">Kategori: <span class="jakarta-b">{{ academic.eventsTheme }}</span></p>
+              <p class="jakarta-r">Jenjang: <span class="jakarta-b">{{ academic.eventsJenjang }}</span></p>
+              <p class="jakarta-r">Lokasi: <span class="jakarta-b">{{ academic.eventsHeld }}</span></p>
+              <p class="jakarta-r">Kapasitas Tersisa: <span class="jakarta-b">{{ academic.capacityTersisa }}</span></p>
+            </div>
+            <q-btn color="primary" icon-right="chevron_right" label="Detail" />
+          </div>
+        </div>
+      </div>
+    </q-page>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue';
+import { api } from 'src/boot/axios';
+import { Notify } from 'quasar';
+
+export default {
+  name: 'OrganizerrHome',
+
+  data() {
+    return {
+      competitionData: [],
+      academicData: [],
+    }
+  },
+
+  methods: {
+    async getCompetitionData() {
+      try {
+        const response = await api.get('viewAllCompetitions')
+        this.competitionData = response.data;
+        console.log("compe", this.competitionData);
+        // if (response.status === 200) {
+        //   this.$router.push('/home');
+        // }
+      } catch (error) {
+        console.log(error);
+        Notify.create({
+          color: 'red',
+          message: 'Gagal mengambil data kompetisi silakan refresh halaman',
+          position: 'top',
+          timeout: 2500
+        });
+      }
+    },
+
+    async getAcademicData() {
+      try {
+        const response = await api.get('viewAllAcademicEvents')
+        this.academicData = response.data;
+        console.log("academic", this.academicData);
+        // if (response.status === 200) {
+        //   this.$router.push('/home');
+        // }
+      } catch (error) {
+        console.log(error);
+        Notify.create({
+          color: 'red',
+          message: 'Gagal mengambil data acara akademik silakan refresh halaman',
+          position: 'top',
+          timeout: 2500
+        });
+      }
+    },
+  },
+
+  async mounted() {
+    await this.getCompetitionData();
+    await this.getAcademicData();
+  }
+}
+</script>
+
+<style scoped>
+
+.add-btn {
+  width: 264px;
+  height: 320px;
+  cursor: pointer;
+}
+
+.card {
+  height: fit-content;
+  width: 264px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  padding: 12px;
+  border-radius: 15px;
+  background: #FAFAFA;
+  box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.04),
+    0px 20px 25px -5px rgba(0, 0, 0, 0.1);
+}
+
+.text-gap {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+}
+
+.card-container {
+  display: flex;
+  gap: 24px;
+  flex-wrap: wrap;
+}
+</style>
--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -43,6 +43,15 @@ const routes = [
     ]
   },
 
+  {
+    path: '/organizer/home',
+    component: () => import('src/layouts/HeaderFooter.vue'),
+    children: [
+      { path: '', name: 'OrganizerHome', component: () => import('pages/admin/Home.vue') },
+    ]
+  },
+
+
 
   // Always leave this as last one,
   // but you can also remove it
